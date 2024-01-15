ab7cb2eeedd1e041cd8e6d21eb11ae374f1885ff
keerthyrao <54347483+keerthyrao@users.noreply.github.com>
2024-01-14 23:26:34 -0600

    Arithmetic Operations

---
+++ Assignment1/Arithmetic.py
@@ -0,0 +1,13 @@
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
+
+add = num1+num2
+sub = num1-num2
+mul = num1*num2
+exponential= num1**num2
+
+
+print("sum of the numbers is:", add)
+print("difference of the numbers is:", sub)
+print("multiplication of the numbers is:", mul)
+print("exponential of the numbers is:", exponential)
